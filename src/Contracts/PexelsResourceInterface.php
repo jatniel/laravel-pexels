a5309f96b066061e5b3302fb5231fb73666c65c5
Jatniel Guzmán <1070744+jatniel@users.noreply.github.com>
2025-12-13 22:07:36 +0100

    Create PexelsResourceInterface.php

---
+++ src/Contracts/PexelsResourceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jatniel\Pexels\Exceptions;
+
+interface PexelsResourceInterface
+{
+    public function getId(): int;
+    public function getUrl(): string;
+    public function toArray(): array;
+}
